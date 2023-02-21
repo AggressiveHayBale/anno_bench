a0db2d4e6175533960f4fc05775a4dc034d44544
Mateusz <mateusz.jun@gmail.com>
2023-02-21 12:59:15 +0100

    noise bugfix

--- bin/noise.sh
+++ bin/noise.sh
@@ -4,11 +4,20 @@ name=$1
 dir=$2 
 noise=$3
 
-awk -v seed=$RANDOM -v noise=$noise 'BEGIN {} !/^>/ {
-    srand(seed)                                        
-    i=int(noise*(length($0)))                   
+  seed="4512"
+  noise_val=$(echo "$noise" | bc)
+  sed -i '/^>/d' ${dir} 
+  awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' ${dir} > lin_fasta.fasta
+  awk -v seed=$seed -v noise_val=$noise_val 'BEGIN {
+    srand(seed)
+  }
+  !/^>/ {
+    fast_len=length($0)
+    i=int(noise_val*fast_len)
     for(j=1;j<=i;j++) {
-        position=int(rand()*length($0))+1             
-        $0=substr($0,1,(position-1)) substr($0,(position+1))  
+      position=int(rand()*length($0))+1
+      $0=substr($0,1,(position-1)) substr($0,(position+1))
     }
-}1' $dir >  noise_${name}.fasta
+  }
+  1' lin_fasta.fasta >  noise_${name}_${noise}.fasta
+  sed -i '1s/^/> contig_1\n/' noise_${name}_${noise}.fasta 
